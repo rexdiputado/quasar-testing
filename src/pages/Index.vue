863baadda76b0841e4e4a47b919c6f4c1fe20765
rexdiputado <jesserex.diputado@gmail.com>
2019-12-19 20:59:14 +0800

    Add files via upload
    trying on different components;second commit

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,38 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page>
     <!-- <img alt="Quasar logo" src="~assets/quasar-logo-full.svg"> -->
-    <div>Quasar Testing</div>
+    <q-list bordered>
+      <q-item
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-avatar size="70px">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+        <q-item-section>Image avatar</q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  created () {
+    this.$axios.get('https://www.reddit.com/r/aww.json?raw_json=1')
+      .then(response => {
+        this.posts = response.data.data.children
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  data () {
+    return {
+      posts: []
+    }
+  }
 }
 </script>
--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -4,10 +4,11 @@
       <div class="col">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum qui voluptatum laborum quasi repudiandae ad sed, doloremque, omnis, accusamus sit consequuntur magni fugit ut explicabo veritatis aliquid. At, magnam dicta!
         <div>
+          <q-btn color="teal" @click="showLoading" label="Show Loading" />
           <q-btn color="primary" label="Primary" @click="handleClick"/>
           <q-input outlined v-model="myInput" label="Outlined" />
         </div>
-        <p v-for="n in 15" :key="n">
+        <p v-for="n in 40" :key="n">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
         </p>
 
@@ -33,6 +34,17 @@ export default {
   methods: {
     handleClick () {
       this.myInput = 'clicked'
+    },
+    showLoading () {
+      this.$q.loading.show({
+        message: 'Some important <b>process</b> is in progress.<br/><span class="text-primary">Hang on...</span>'
+      })
+
+      // hiding in 3s
+      this.timer = setTimeout(() => {
+        this.$q.loading.hide()
+        this.timer = void 0
+      }, 3000)
     }
   }
 }
